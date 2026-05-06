a33624cc54c7cd2ee58082f87382294f9340c0f8
RafaelBassalo <rafaelbassalo@gmail.com>
2026-05-06 09:50:43 -0300

    adicionando comentarios

--- AULA 2/aula2.js
+++ AULA 2/aula2.js
@@ -27,3 +27,14 @@
 console.log(meuArray)
 */
 
+const enviarCidade = () => {
+            const cidadeInput = document.getElementById('cidade') //pegando o valor do input
+            const cidade = cidadeInput.value //armazenando o valor do input em uma variável
+            const populacaoInput = document.getElementById('populacao') //pegando o valor do input
+            const populacao = populacaoInput.value //armazenando o valor do input em uma variável
+            if (populacao >= 300000) {  //verificando se a população é maior ou igual a 300.000
+                alert(`A cidade ${cidade} é considerada uma cidade grande.`) //exibindo um alerta com a mensagem
+            } else { //caso contrário, exibindo um alerta com a mensagem
+                alert(`A cidade ${cidade} é considerada uma cidade pequena.`)
+            }
+        }
